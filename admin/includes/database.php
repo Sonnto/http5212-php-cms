7f8406a5f400659e99d760b9e4eaeed3bea0ea93
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-02-15 16:23:49 -0500

    init, add employment skeleton page

---
+++ admin/includes/database.php
@@ -0,0 +1,10 @@
+<?php
+
+$connect = mysqli_connect( 
+    "sql200.epizy.com", //host 
+    "epiz_33366090", //username 
+    "ztp5toRTZu61M", //password
+    "epiz_33366090_cms" //database name 
+);
+
+mysqli_set_charset( $connect, 'UTF8' );
