30af7b5c85749611abe52e338f6a7c5f04bf5ff9
Sonnto <90372802+Sonnto@users.noreply.github.com>
2023-03-23 17:27:08 -0400

    fix preview for skills

--- admin/skills.php
+++ admin/skills.php
@@ -48,7 +48,7 @@ $result = mysqli_query( $connect, $query );
   <?php while( $record = mysqli_fetch_assoc( $result ) ): ?>
     <tr>
       <td align="center">
-        <img src="image.php?type=project&id=<?php echo $record['id']; ?>&width=300&height=300&format=inside">
+        <img src="image.php?type=skill&id=<?php echo $record['id']; ?>&width=300&height=300&format=inside">
       </td>
       <td align="center"><?php echo $record['id']; ?></td>
       <td align="left">
